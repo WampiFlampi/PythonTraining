9677360136d3d9d706be785d8a48c852c8e5ce48
Wampi Flampi <32463657+WampiFlampi@users.noreply.github.com>
2017-11-02 21:35:33 -0400

    Update exercise1.py

--- exercise1.py
+++ exercise1.py
@@ -1,4 +1,10 @@
-wampi=input('Type your Name as a String')
-flampi=input('Type your Age')
+name=input('Type your Name as a String')
+age=input('Type your Age')
 currentyear=2017
 def grumpus(): 
+  birthday=currentyear-age+100
+  print(name+' will turn 100 on'+ str(birthday)
+ 
+        
+grumpus()
+  

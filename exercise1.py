8261ef68dcc5401c9477ea32848c6ccb44d0414f
Wampi Flampi <32463657+WampiFlampi@users.noreply.github.com>
2017-11-09 19:32:30 -0500

    Update exercise1.py

--- exercise1.py
+++ exercise1.py
@@ -2,7 +2,7 @@ name=input('Type your Name as a String')
 age=input('Type your Age')
 currentyear=2017
 birthday=currentyear-age+100
-print(name+' will turn 100 on'+ str(birthday)
+print(name+' will turn 100 on'+ str(birthday))
  
         
   

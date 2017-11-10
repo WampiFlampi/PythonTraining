c1ae76b6fab2541ecacb7db0e67e53951bfc5c78
Wampi Flampi <32463657+WampiFlampi@users.noreply.github.com>
2017-11-09 19:46:25 -0500

    Update exercise1.py

--- exercise1.py
+++ exercise1.py
@@ -1,8 +1,10 @@
-name=input('Type your Name as a String')
-age=input('Type your Age')
-currentyear=2017
-birthday=currentyear-age+100
-print(name+' will turn 100 on'+ str(birthday))
- 
+def florp():
+    name=input('Type your Name as a String')
+    age=input('Type your Age')
+    currentyear=2017
+    birthday=currentyear-int(age)+100
+    print(name+' will turn 100 in '+ str( birthday))
+florp()
+
         
   

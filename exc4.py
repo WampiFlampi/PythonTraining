6e0e443ec8ad6b23b9515e1f93db9c4539638ce7
Wampi Flampi <32463657+WampiFlampi@users.noreply.github.com>
2017-11-13 19:59:26 -0500

    Update exc4.py

--- exc4.py
+++ exc4.py
@@ -1,6 +1,12 @@
 def yes():
     x = input("Enter any interger to see it's divisors:")
     a = range(1,int(x)+1)
+    b = range(int(x)*-1-1,-1)
+    for elem in b:
+        g=int(x)%elem
+        if g==0:
+            print (elem)
+
     for elem in a:
         y=int(x)%elem
         if y==0:

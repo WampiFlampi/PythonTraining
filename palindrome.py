2330b6b06875c7914816894d460b5cee59f4dd17
Wampi Flampi <32463657+WampiFlampi@users.noreply.github.com>
2017-11-19 22:17:04 -0500

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,6 +1,28 @@
-def y():
-  string=input("string")
-  x= (string[::-1])
-  if x==string:
-   print ('Palindrome')
-  
+#Homemade Palindrome Checker
+b=0
+a=input("a word:")
+list1=[]
+for elem in a:
+    b=b+1
+for elem in a:
+    b=b-1
+    q=a[b]
+    list1.append(q)
+list1=''.join(list1)
+if a ==list1:
+    print ("palindrome")
+else:
+    print ("sadness")
+
+
+
+
+
+#Cheaty disgusting phil version
+    
+#a=input('any word:')
+#x=a[::-1]
+#if a==x:
+    #print ('palindrome')
+#else:
+    #print ('sadness')

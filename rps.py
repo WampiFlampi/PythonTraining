a061f357afa93f7667937f29eaea4184f7e41a1a
HonoriaShana <33732069+HonoriaShana@users.noreply.github.com>
2017-11-28 11:59:02 -0500

    Create rps.py
    https://repl.it/repls/PurpleBoilingAnophelesmosquito

---
+++ rps.py
@@ -0,0 +1,16 @@
+print('Rock Paper Scissors Time!')
+user = input('Put in your move! (Rock, Paper, or Scissors)')
+if user == "Rock":
+  print("Paper")
+elif user == "Paper":
+  print("Scissors")
+elif user == "Scissors":
+  print("Rock")
+elif user == "scissors":
+  print("Rock")
+elif user == "paper":
+  print("Scissors")
+elif user =="rock":
+  print("paper")
+print("Congratulations robot! Want to play again?")
+choice = input("Want to play again? (Yes or No)")

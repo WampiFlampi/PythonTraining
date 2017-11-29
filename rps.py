48c4427572fba46e1d96696936b5b7191a6dc75a
Basil <matthewbaisley@outlook.com>
2017-11-28 19:41:30 -0500

    This actually works now!

--- rps.py
+++ rps.py
@@ -1,24 +1,29 @@
-print('Rock Paper Scissors Time!')
-infinite == notyes
-user = input('Put in your move! (Rock, Paper, or Scissors)')
-if user == "Rock":
-  print("Paper")
-elif user == "Paper":
-  print("Scissors")
-elif user == "Scissors":
-  print("Rock")
-elif user == "scissors":
-  print("Rock")
-elif user == "paper":
-  print("Scissors")
-elif user =="rock":
-  print("paper")
-print("Congratulations robot! Want to play again?")
-choice = input("Want to play again? (Yes or No)")
-if choice == "no":
-  print("Ok!")
-elif choice == "No":
-  print("Ok!")
-elif choice == "yes or Yes":
-  while choice != infinite:
-    #https://repl.it/repls/PurpleBoilingAnophelesmosquito
+import random
+stop = False
+print("Rock Paper Scissors Time!")
+while stop == False:
+  opponent = random.choice(["Rock","Paper","Scissors"])
+  user = input('Put in your move! (Rock, Paper, or Scissors)').lower()
+  if user == "Rock" or "rock" and opponent == "Scissors":
+    print("The opponent picked Scissors! You win!")
+  elif user == "Rock" or "rock" and opponent == "Paper":
+    print("The opponent picked Paper! You lose!")
+  elif user == "Rock" or "rock" and opponent == "Rock":
+    print("The opponent picked Rock as well! You tie!")
+  elif user == "Paper" or "paper" and opponent == "Scissors":
+    print("The opponent picked Scissors! You lose!")
+  elif user == "Paper" or "paper" and opponent =="Rock":
+    print("The opponent picked Rock! You win!")
+  elif user == "Paper" or "paper" and opponent == "Paper":
+    print("The opponent picked Paper as well! You tie!")
+  elif user == "Scissors" or "scissors" and opponent == "Paper":
+    print("The opponent picked Paper! You win!")
+  elif user == "Scissors" or "scissors" and opponent == "Rock":
+    print("The opponent picked Rock! You win!")
+  elif user =="Scissors" or "scissors" and opponent == "Scissors":
+    print("The opponent picked Scissors as well! You tie!")
+  chk = input('Would you like to play again? (Yes, No)').lower()
+  if chk == "yes":
+    stop = False
+  else:
+    stop = True
